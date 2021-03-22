e983f91c1b61cc0963d3f3029d0708f8992385fa
Oscar Cornejo <oscarcornejo10@gmail.com>
2021-03-22 11:09:02 -0300

    Config ok

---
+++ app.js
@@ -0,0 +1,52 @@
+const { inquirerMenu, leerInput, pausa } = require('./helpers/inquirer');
+const { sumar, restar, multiplicar, dividir } = require('./calculator');
+
+const main = async () => {
+  let opt = '';
+
+  do {
+    // Imprimir el Menú
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case '1':
+        // Sumar
+        const sum1 = await leerInput('Primer Valor:');
+        const sum2 = await leerInput('Segundo Valor:');
+        console.log();
+        console.log('El resultado de la suma es: ', sumar(Number(sum1), Number(sum2)));
+        break;
+
+      case '2':
+        // Restar
+        const rest1 = await leerInput('Primer Valor:');
+        const rest2 = await leerInput('Segundo Valor:');
+        console.log();
+        console.log('El resultado de la resta es: ', restar(Number(rest1), Number(rest2)));
+        break;
+
+      case '3':
+        // Multiplicar
+        const mult1 = await leerInput('Primer Valor:');
+        const mult2 = await leerInput('Segundo Valor:');
+        console.log();
+        console.log('El resultado de la multiplicación es: ', multiplicar(Number(mult1), Number(mult2)));
+        break;
+
+      case '4':
+        // Dividir
+        const div1 = await leerInput('Primer Valor:');
+        const div2 = await leerInput('Segundo Valor:');
+        console.log();
+        console.log('El resultado de la división es: ', dividir(Number(div1), Number(div2)));
+        break;
+    }
+
+    // TODO: Guardar en DB
+    // guardarDB(resultado);
+
+    if (opt !== '0') await pausa();
+  } while (opt !== '0');
+};
+
+main();
---
+++ helpers/inquirer.js
@@ -0,0 +1,85 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const preguntas = [
+  {
+    type: 'list',
+    name: 'opcion', // Nombre a destructurar para retornar (ver línea 27)
+    message: '¿Qué operación desea realizar?',
+    choices: [
+      { value: '1', name: `${'1.'.green} Sumar` },
+      { value: '2', name: `${'2.'.green} Restar` },
+      { value: '3', name: `${'3.'.green} Multiplicar` },
+      { value: '4', name: `${'4.'.green} Dividir` },
+      { value: '0', name: `${'0.'.green} Salir` },
+    ],
+  },
+];
+
+const inquirerMenu = async () => {
+  console.clear();
+  console.log('========================'.green);
+  console.log(' Selecione una opción '.white);
+  console.log('========================\n'.green);
+
+  const { opcion } = await inquirer.prompt(preguntas); /* Pass your questions in here */
+
+  return opcion;
+};
+
+const pausa = async () => {
+  const question = [
+    {
+      type: 'input',
+      name: 'enter',
+      message: `Presione ${'Enter'.green} para continuar`,
+    },
+  ];
+  console.log('\n');
+  await inquirer.prompt(question);
+};
+
+const leerInput = async (mensaje) => {
+  //   const onlyNumber = new RegExp('^[0-9]+$');
+  const onlyNumber = new RegExp('^[0-9]{1,2}([.][0-9]{1,2})?$');
+
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message: mensaje,
+      validate(value) {
+        if (value.length === 0) {
+          return 'Por favor ingrese un valor';
+        } else if (!onlyNumber.test(value)) {
+          return 'Por favor ingrese un valor numérico';
+        }
+        return true;
+      },
+    },
+  ];
+
+  const { desc } = await inquirer.prompt(question);
+  return desc;
+};
+
+const confirmar = async (message) => {
+  const question = [
+    {
+      type: 'confirm',
+      name: 'ok',
+      message,
+    },
+  ];
+
+  const { ok } = await inquirer.prompt(question);
+
+  return ok;
+};
+
+module.exports = {
+  inquirerMenu,
+  pausa,
+  leerInput,
+  confirmar,
+};
